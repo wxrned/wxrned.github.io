3a93940458bf2ca6fbf088ebc9c76877209c4cf2
warn <wxrn.lol@gmail.com>
2024-09-26 12:01:55 +0200

    Update updateMetaTags.js

--- updateMetaTags.js
+++ updateMetaTags.js
@@ -1,5 +1,10 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Get the current directory name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 async function getFetch() {
     const fetch = await import('node-fetch');
