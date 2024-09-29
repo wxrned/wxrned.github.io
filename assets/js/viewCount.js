a9dcbe69d2eb4b7ed768e0b5da0f528a47753745
warn <wxrn.lol@gmail.com>
2024-09-29 18:58:44 +0200

    Update viewCount.js

--- assets/js/viewCount.js
+++ assets/js/viewCount.js
@@ -1,3 +1,18 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCTzKMUnEqwoEiiYN-NEqZO5fbcUPJFYxY",
+  authDomain: "wxrnlol-eb507.firebaseapp.com",
+  databaseURL:
+    "https://wxrnlol-eb507-default-rtdb.europe-west1.firebasedatabase.app/",
+  projectId: "wxrnlol-eb507",
+  storageBucket: "wxrnlol-eb507.appspot.com",
+  messagingSenderId: "130600299639",
+  appId: "1:130600299639:web:8c24f992f6be60898a6a72",
+  measurementId: "G-4MHBKXVH15",
+};
+
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+
 function get_viewers_ip(json) {
   let ip = json.ip;
 
@@ -36,27 +51,25 @@ function enterSite() {
 }
 
 function countViews(ip) {
-  fetch("https://api.wxrn.lol/api/views", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "x-api-key": "kJ3ytkb85tnltEGmZ7dHE62IjGbWgYhTeUNEe18xJZandErLhHxj7EkQX1r4ocxc",
-    },
-    body: JSON.stringify({ ip }),
-  })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    return response.json();
-  })
-  .then(data => {
-    const views = data.views;
-    animateCountUp(views);
-  })
-  .catch(error => {
-    console.error("Error recording view:", error);
-  });
+  var views;
+  var ip_to_string = ip.toString().replace(/\./g, "-");
+
+  firebase
+    .database()
+    .ref()
+    .child("page_views/" + ip_to_string)
+    .set({
+      viewers_ip: ip,
+    });
+
+  firebase
+    .database()
+    .ref()
+    .child("page_views")
+    .on("value", function (snapshot) {
+      views = snapshot.numChildren();
+      animateCountUp(views);
+    });
 }
 
 fetch("https://api.ipify.org/?format=json")
@@ -77,18 +90,18 @@ fetch("https://api.ipify.org/?format=json")
 function animateCountUp(targetNumber) {
   const pageViewsElement = document.getElementById("page_views");
   const currentNumber = parseInt(pageViewsElement.innerHTML);
-  const increment = Math.ceil((targetNumber - currentNumber) / 100);
-  const duration = 1000;
-  const steps = Math.ceil(duration / 50);
+  const increment = Math.ceil((targetNumber - currentNumber) / 100); // Increment step
+  const duration = 1000; // Duration of the animation in milliseconds
+  const steps = Math.ceil(duration / 50); // Number of steps for the animation
   let count = currentNumber;
 
   const interval = setInterval(() => {
     count += increment;
     if (increment > 0 && count >= targetNumber) {
-      count = targetNumber;
+      count = targetNumber; // Stop at target number
       clearInterval(interval);
     } else if (increment < 0 && count <= targetNumber) {
-      count = targetNumber;
+      count = targetNumber; // Stop at target number
       clearInterval(interval);
     }
     pageViewsElement.innerHTML = count;
